40b0505def8b65480f8133d3d69e9d0bec461187
Itoafa Liviu <Itoafa Liviu@localhost>
2011-06-09 09:50:14 +0300

    fix p27

--- p27.py
+++ p27.py
@@ -14,18 +14,20 @@ def is_prime(n):
 
 def solve():
     max_num_primes = 0
+    max_a = 0
+    max_b = 0
     for a in range(-999, 1000):
         for b in range(-999, 1000):
             num_primes = 0
             n = 0
             while n*n+n*a+b>0 and is_prime(n*n+n*a+b):
                 n += 1
-                num_primes += 1
-            if a==1 and b==41:
-                print num_primes
+                num_primes += 1         
             if num_primes>max_num_primes:
                 max_num_primes = num_primes
-    print "seq len %d a=%d b=%d a*b=%d" %(max_num_primes, a, b, a*b)
+                max_a = a
+                max_b = b
+    print "seq len %d a=%d b=%d a*b=%d" %(max_num_primes, max_a, max_b, max_a*max_b)
     
 if __name__=="__main__":
     solve()
