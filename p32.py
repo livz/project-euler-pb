1da2f00e921fafe9acad671e48b5183ac44ca608
Liviu <Liviu@localhost>
2011-06-09 20:22:48 +0300

    solved p32.py

--- p32.py
+++ p32.py
@@ -3,26 +3,23 @@
 # Find the sum of all products whose multiplicand/multiplier/product 
 # identity can be written as a 1 through 9 pandigital.
 
-def all_perms(str):
-    if len(str) <=1:
-        yield str
-    else:
-        for perm in all_perms(str[1:]):
-            for i in range(len(perm)+1):
-                #nb str[0:1] works in both string and list contexts
-                yield perm[:i] + str[0:1] + perm[i:]
+import itertools
 
 def solve():
-    perm = all_perms([1,2,3,4,5,6,7,8,9])
-    list_p = list(perm)    
-    
-    for p in list_p:
-        for i in range(0,8):
-            for j in range(0,8):
-                if i+j<=8:
-                    n1=0
-                    while i>0:
-                        n1 +=p
+    perm = itertools.permutations([1,2,3,4,5,6,7,8,9])
+
+    list_prod = []
+    for p in list(perm):
+        for i in range(1,9):
+            for j in range(i+1,9):
+                if j<=8:
+                    a = reduce(lambda x,y: x*10+y, p[:i])
+                    b = reduce(lambda x,y: x*10+y, p[i:j])
+                    c = reduce(lambda x,y: x*10+y, p[j:9])
+                    if (a*b==c):                        
+                        list_prod.append(c)
+    print set(list_prod)
+    print reduce(lambda x,y:x+y, set(list_prod))
         
 if __name__=="__main__":
     solve()
