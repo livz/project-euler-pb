e471b86f3c64fabf9780452b8c02a4c67086ce6e
Liviu <Liviu@localhost>
2011-06-10 08:22:39 +0300

    ...

--- p230.py
+++ p230.py
@@ -3,11 +3,13 @@
 A = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
 B = "8214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196"
 
+len_term = 100
+
 def fab(n):
     a = 1
     b = 1
     prev_a = a
-    while b*100<n:
+    while b*len_term<n:
         prev_a = a
         tmp = a
         a = b
@@ -16,17 +18,26 @@ def fab(n):
     return (prev_a,a)
 
 def red(n):
-    while n>100:
-        print fab(n)
-        n -= (fab(n)[0]*100)
-    return n
+    term = 0
+    
+    while n>len_term:
+        term += 1
+        n -= (fab(n)[0]*len_term)
+        
+    if (term%2 == 0):
+        return A[n-1]
+    else:
+        return B[n-1]
     
 def solve():
+    sum = 0
+    
     for i in range(0, 18):
         x = (127+19*i)*(7**i)
-        print x
-        
+        sum += (10**i) * int(red(x))
+        print "%d %d" % (x, int(red(x)))
+    print sum
+    
 if __name__=="__main__":
-    #solve()
-    print red(360)
+    solve()
     
